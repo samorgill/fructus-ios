073910af3337822c56edb5ae6dbb4d903dbf8494
Sam Orgill <ssorgill@icloud.com>
2020-10-20 14:59:34 +0100

    Cards & onboarding screen

---
+++ Fructus/App/OnboardingVIew.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingVIew.swift
+//  Fructus
+//
+//  Created by Sam Orgill on 20/10/2020.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
