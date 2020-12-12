4a55a68ce95bf90425d5c6bd62c790a06301803a
Sam Orgill <ssorgill@icloud.com>
2020-12-12 16:57:38 +0000

    iPad navigation

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     // MARK: - PROPERTIES
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     // MARK: - BODY
     
@@ -31,18 +32,49 @@ struct SettingsView: View {
                                 .scaledToFit()
                                 .frame(width: 80, height: 80)
                                 .cornerRadius(9)
-                    
+                            
                             Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterol. Fruits are sources of many essental nutrients, including potassium, dietry fiber, vitamins, and much more.")
                                 .font(.footnote)
                         }
                     }
                     // MARK: - SECTION 2
                     
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customisation", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("You can restart this application by toggling this switch in this box")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                        
+                    }
+                    
                     // MARK: - SECTION 3
                     
                     GroupBox(
                         label:
-                        SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                            SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
                     ) {
                         SettingsRowView(name: "Developer", content: "Sam Orgill")
                         SettingsRowView(name: "Compatability", content: "iOS 14")
