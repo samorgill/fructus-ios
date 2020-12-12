bc5b3c9ecc3a15cbe59bc4d035ff21a1be1eb2fd
Sam Orgill <ssorgill@icloud.com>
2020-12-12 15:25:29 +0000

    Settings sheet

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Sam Orgill on 12/12/2020.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                }
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark")
+                        }
+                )
+                .padding()
+            } //: SCROLL
+        } //: - NAVIGATION
+        
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
