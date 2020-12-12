1ff80b4a0706a7cb1bd37b063be415e08e828b8e
Sam Orgill <ssorgill@icloud.com>
2020-12-12 11:39:19 +0000

    Fruit detail, header & nutrition

---
+++ Fructus/View/SourceLinkView.swift
@@ -0,0 +1,28 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Sam Orgill on 12/12/2020.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox() {
+            HStack {
+                Text("Content source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+        }
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+    }
+}
